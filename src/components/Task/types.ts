47a4021441dace0e58347a176b6ec1f6f8ea24c7
SHDGMSQ <sh.dg.msq@mail.ru>
2024-07-06 15:12:57 +0300

    add main logic of app

--- src/components/Task/types.ts
+++ src/components/Task/types.ts
@@ -6,9 +6,14 @@ export interface TaskType {
   id: string;
   title: string;
   isDone: boolean;
+
 }
 
 export interface TaskPropsType {
   title: string;
   isDone: boolean;
+  id: string;
+  todoId: string;
+  changeTaskStatus: (todoId: string, taskId: string, status: boolean) => void;
+  removeTask: (todoId: string, taskId: string) => void;
 }
--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,32 +1,64 @@
-import {memo} from "react";
+import {memo, useCallback} from "react";
 import {Task} from "@/components/Task/Task";
-import styles from "./Todolist.module.scss"
+import styles from "./Todolist.module.scss";
 import {TodolistPropsType} from "@/components/Todolist/types";
 import {AddItemForm} from "@/components/AddItemForm/AddItemForm";
 
 export const Todolist = memo((props: TodolistPropsType) => {
-  const {title, tasks} = props;
+  const {id, title, tasks, removeTodolist, addTask, changeTaskStatus, changeTasks, filter, removeTask} = props;
+
+  const removeTodolistHandler = useCallback(() => {
+    removeTodolist(id);
+  }, [removeTodolist, id]);
+
+  const addTaskHandler = useCallback((title: string) => {
+    addTask(id, title);
+  }, [addTask, id]);
+
+  const onAllClickHandler = useCallback(() => {
+    changeTasks(id, "All");
+  }, [changeTasks, id]);
+  const onActiveClickHandler = useCallback(() => {
+    changeTasks(id, "Active");
+  }, [changeTasks, id]);
+  const onCompletedClickHandler = useCallback(() => {
+    changeTasks(id, "Completed");
+  }, [changeTasks, id]);
+
+  let tasksForTodolist = tasks;
+
+  if (filter === "Active") {
+    tasksForTodolist = tasksForTodolist.filter(task => !task.isDone);
+  }
+
+  if (filter === "Completed") {
+    tasksForTodolist = tasksForTodolist.filter(task => task.isDone);
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.todoHeader}>
         <h3>{title}</h3>
-        <button>x</button>
+        <button onClick={removeTodolistHandler}>x</button>
       </div>
-     <AddItemForm addItem={() => {}}/>
+      <AddItemForm addItem={addTaskHandler}/>
       {
-        tasks.map((task) =>
+        tasksForTodolist.map((task) =>
           <Task
             key={task.id}
+            id={task.id}
+            todoId={id}
             title={task.title}
             isDone={task.isDone}
+            changeTaskStatus={changeTaskStatus}
+            removeTask={removeTask}
           />
         )
       }
       <div className={styles.btnsContainer}>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={onAllClickHandler} className={`${filter === "All" ? styles.chosenButton : ""}`}>All</button>
+        <button onClick={onActiveClickHandler} className={`${filter === "Active" ? styles.chosenButton : ""}`}>Active</button>
+        <button onClick={onCompletedClickHandler} className={`${filter === "Completed" ? styles.chosenButton : ""}`}>Completed</button>
       </div>
     </div>
   );
